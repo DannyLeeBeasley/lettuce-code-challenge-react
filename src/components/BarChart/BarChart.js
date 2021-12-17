29556c53342f85745adfaa1348b372ee36557b88
Dan Beasley <dannyleebeasley@gmail.com>
2021-12-17 13:16:31 -0600

    Got a blessed graph on the blessed page

--- src/components/BarChart/BarChart.js
+++ src/components/BarChart/BarChart.js
@@ -1,52 +1,66 @@
 import React, { useState } from "react";
-import { Doughnut, Bar, Pie } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import faker from "faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 function BarChart() {
-    
-  // let worldCitiesPopDensity = {
-  //     chartData: {
-  //       labels: [
-  //         "Manila, Philippines",
-  //         "Pateros, Philippines",
-  //         "Mandaluyong, Philippines",
-  //         "Baghdad, Iraq",
-  //         "Mumbai, India",
-  //         "Dhaka, Bangladesh",
-  //         "Caloocan, Philippines",
-  //         "Port-au-Prince, Haiti",
-  //         "Bnei Brak, Israel",
-  //         "Levallois-Perret, France",
-  //       ],
-  //       datasets: [
-  //         {
-  //           label: "Population",
-  //           data: [
-  //             119600, 94400, 90460, 85140, 83660, 75290, 72490, 70950, 70810,
-  //             68460,
-  //           ],
-  //           backgroundColor:[
-  //               "red", "orange", "yellow", "green", "blue", "purple"
-  //           ]
-  //         },
-  //       ],
-  //     },
-  //   };
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "World Cities Population Density",
+      },
+    },
+  };
 
-  //   const [chartData, setChartData] = useState(worldCitiesPopDensity);
-  
-    return (
-      <div className="chart">
-        <h1>HEY!!!</h1>
-        {/* <Bar
-          data={chartData}
-          width={100}
-          height={50}
-          options={{
-            maintainAspectRatio: false,
-          }}
-        /> */}
-      </div>
-    );
-  }
+  const labels = [
+    "Manila, Philippines",
+    "Pateros, Philippines",
+    "Mandaluyong, Philippines",
+    "Baghdad, Iraq",
+    "Mumbai, India",
+    "Dhaka, Bangladesh",
+    "Caloocan, Philippines",
+    "Port-au-Prince, Haiti",
+    "Bnei Brak, Israel",
+    "Levallois-Perret, France",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Persons / Square Mile",
+        data: [
+          119600, 94400, 90460, 85140, 83660, 75290, 72490, 70950, 70810, 68460,
+        ],
+        backgroundColor: ["red", "orange", "yellow", "green", "blue", "purple"],
+      },
+    ],
+  };
+
+  return <Bar options={options} data={data} />;
+}
 
 export default BarChart;
